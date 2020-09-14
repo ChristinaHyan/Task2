3509bf93c568ead8c10669d558cb2003527509ab
Kristina Hunanyan <43448450+ChristinaHyan@users.noreply.github.com>
2020-09-15 00:18:01 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const PI = Math.PI;
+let r = parseInt(prompt('Please enter here the radius of the circle.'));
+let s = PI * r**2;
+let p = 2 * PI * r;
+
+console.log(`The Area of our Circle is: ${s}.`);
+console.log(`The Perimeter of our Circle is: ${p}.`);
